4f25abf787a17a76349b93c9d132f6874ac154c8
Cynthia Escano <92443398+warmheartsministries@users.noreply.github.com>
2024-09-19 14:10:26 -0400

    included scan link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -47,6 +48,15 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div>
+          <Link
+            href={{
+              pathname: "/scan",
+            }}
+          >
+            SCAN
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
