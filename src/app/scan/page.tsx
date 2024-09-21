7c4ce722ffb9b8daefbff9fbc90e7b908948a37a
Cynthia Escano <92443398+warmheartsministries@users.noreply.github.com>
2024-09-21 10:18:16 -0400

    fixed searparams

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,10 +1,14 @@
 import { BarcodeScanner } from "../ui/barcode-scanner";
 
-export default async function Page({ query }: { query: string }) {
+export default async function Page({ searchParams }: { searchParams: string }) {
+  const query = searchParams || "";
+  console.log("PAGE query: ", query);
+  const qKey = Object.keys(query)[0];
+  const qValue = Object.values(query)[0];
   return (
     <>
-      {Object.values(query)[0] ? (
-        <div>I got: {Object.values(query)[0]} to fetch...</div>
+      {qKey && qValue ? (
+        <div>I got: {qValue} to fetch...</div>
       ) : (
         <BarcodeScanner />
       )}
